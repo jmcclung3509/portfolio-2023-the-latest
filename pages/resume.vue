f8af3acdf9b4a0bcb1db00deaf635e98330e0054
Jessica Turner <jmcclung3509@gmail.com>
2023-12-06 10:07:31 -0800

    Style edits

--- pages/resume.vue
+++ pages/resume.vue
@@ -3,7 +3,7 @@
 		<div class="container mx-auto space-y-14">
 			<div class="header space-y-2">
 				<h1 class="text-4xl">Jessica Turner</h1>
-				<h2 class="text-1.5xl">Frontend Developer</h2>
+				<h2 class="text-2xl">Frontend Developer</h2>
 				<div class="links flex gap-4 justify-start">
 					<nuxt-link
 						class="caps text-dark-purple hover:text-orange animate-underline orange"
@@ -23,8 +23,8 @@
 				</div>
 			</div>
 			<div class="objective flex flex-col items-start space-y-4">
-				<h3 class="font-bold">Objective</h3>
-				<p>
+				<h3 class="font-bold text-[20px]">Objective</h3>
+				<p class="font-light text-dark-purple">
 					Experienced Frontend Developer specializing in Vue.js, Nuxt,
 					and WordPress, with a proven track record of successfully
 					delivering high-quality web pages and applications.
@@ -35,84 +35,84 @@
 				</p>
 			</div>
 			<div class="skills flex flex-col items-start space-y-4">
-				<h3 class="font-bold">Skills</h3>
+				<h3 class="font-bold text-[20px]">Skills</h3>
 				<div class="flex flex-col items-start space-y-1">
 					<h4 class="caps">Technologies & Languages</h4>
 					<div class="skill-container flex flex-wrap">
-						<span class="mr-4 mb-1 text-dark-purple font-extralight">Vue 2/3</span>
-						<span class="mr-4 mb-1 text-dark-purple font-extralight">Nuxt 2/3</span>
-						<span class="mr-4 mb-1 text-dark-purple font-extralight">Wordpres</span>
-						<span class="mr-4 mb-1 text-dark-purple font-extralight">React</span>
-						<span class="mr-4 mb-1 text-dark-purple font-extralight">Gulp</span>
-						<span class="mr-4 mb-1 text-dark-purple font-extralight"
+						<span class="mr-4 mb-1 text-dark-purple font-light">Vue 2/3</span>
+						<span class="mr-4 mb-1 text-dark-purple font-light">Nuxt 2/3</span>
+						<span class="mr-4 mb-1 text-dark-purple font-light">Wordpres</span>
+						<span class="mr-4 mb-1 text-dark-purple font-light">React</span>
+						<span class="mr-4 mb-1 text-dark-purple font-light">Gulp</span>
+						<span class="mr-4 mb-1 text-dark-purple font-light"
 							>CSS Animation</span
 						>
-						<span class="mr-4 mb-1 text-dark-purple font-extralight">Azure</span>
-						<span class="mr-4 mb-1 text-dark-purple font-extralight"
+						<span class="mr-4 mb-1 text-dark-purple font-light">Azure</span>
+						<span class="mr-4 mb-1 text-dark-purple font-light"
 							>SourceTree</span
 						>
-						<span class="mr-4 mb-1 text-dark-purple font-extralight"
+						<span class="mr-4 mb-1 text-dark-purple font-light"
 							>Git/GitHub</span
 						>
-						<span class="mr-4 mb-1 text-dark-purple font-extralight"
+						<span class="mr-4 mb-1 text-dark-purple font-light"
 							>FTP Client</span
 						>
-						<span class="mr-4 mb-1 text-dark-purple font-extralight"
+						<span class="mr-4 mb-1 text-dark-purple font-light"
 							>Adobe Creative Suite</span
 						>
-						<span class="mr-4 mb-1 text-dark-purple font-extralight">Figma</span>
-						<span class="mr-4 mb-1 text-dark-purple font-extralight">HubSpot</span>
-						<span class="mr-4 mb-1 text-dark-purple font-extralight">SEO</span>
+						<span class="mr-4 mb-1 text-dark-purple font-light">Figma</span>
+						<span class="mr-4 mb-1 text-dark-purple font-light">HubSpot</span>
+						<span class="mr-4 mb-1 text-dark-purple font-light">SEO</span>
 					</div>
 				</div>
 				<h4 class="caps">Soft Skills</h4>
 				<div class="skill-container flex flex-wrap">
-					<span class="mr-4 mb-1 text-dark-purple font-extralight"
+					<span class="mr-4 mb-1 text-dark-purple font-light"
 						>Creative problem solving</span
 					>
-					<span class="mr-4 mb-1 text-dark-purple font-extralight">Adaptable</span>
-					<span class="mr-4 mb-1 text-dark-purple font-extralight"
+					<span class="mr-4 mb-1 text-dark-purple font-light">Adaptable</span>
+					<span class="mr-4 mb-1 text-dark-purple font-light"
 						>Efficient time-management</span
 					>
-					<span class="mr-4 mb-1 text-dark-purple font-extralight"
+					<span class="mr-4 mb-1 text-dark-purple font-light"
 						>Detail-oriented</span
 					>
-					<span class="mr-4 mb-1 text-dark-purple font-extralight"
+					<span class="mr-4 mb-1 text-dark-purple font-light"
 						>Growth mindset</span
 					>
-					<span class="mr-4 mb-1 text-dark-purple font-extralight"
+					<span class="mr-4 mb-1 text-dark-purple font-light"
 						>Can-do attitude</span
 					>
 				</div>
 			</div>
 			<div class="experience flex flex-col items-start space-y-7">
-				<h3 class="font-bold">Experience</h3>
+				<h3 class="font-bold text-[20px]">Experience</h3>
 				<div class="flex flex-col space-y-2">
 					<h3>
 						SiteZeus | Junior Frontend Developer | September 2022-
 						Present
 					</h3>
 					<ul class="resume space-y-4">
-						<li class="text-dark-purple font-extralight">
+						<li class="text-dark-purple font-light">
 							Successfully converted legacy WordPress pages to
 							Vue/Nuxt <a href = "https://sitezeus.com">  webpages </a> improving performance and
 							enhancing user experience.
 						</li>
-						<li class="text-dark-purple font-extralight">
+						<li class="text-dark-purple font-light">
 							Maintained existing <a href="https://insites.sitezeus.com">WordPress </a> website while adding over 30+ new pages.
 						</li>
-						<li class="text-dark-purple font-extralight">
+						<li class="text-dark-purple font-light">
 							Optimize website performance by minimizing load
 							times. Implementing lazy loading, and reducing
 							resource requests, resulting in improved user
 							satisfaction and SEO rankings.
 						</li>
-						<li class="text-dark-purple font-extralight">
+						<li class="text-dark-purple font-light">
 							Create and maintain documentation for code, project
 							architectures, and coding standards, facilitating
 							seamless knowledge transfer for team members.
 						</li>
-						<li class="text-dark-purple font-extralight">
+						<li class="text-dark-purple font-light">
 							Troubleshoot and debug frontend issues efficiently,
 							employing browser developer tools and industry best
 							standards to maintain high-quality code.
@@ -124,21 +124,21 @@
 						Freelance | Frontend Developer | January 2021- Present
 					</h3>
 					<ul class="resume space-y-4">
-						<li class="text-dark-purple font-extralight">
+						<li class="text-dark-purple font-light">
 							Create wireframes, prototypes, and mockups using
 							Adobe Illustrator and Figma to visualize and test
 							design concepts.
 						</li>
-						<li class="text-dark-purple font-extralight">
+						<li class="text-dark-purple font-light">
 							Leveraged Vue, Nuxt and React frontend frameworks to
 							construct dynamic websites and applications.
 						</li>
-						<li class="text-dark-purple font-extralight">
+						<li class="text-dark-purple font-light">
 							Proficiently build and sustain dynamic, responsive
 							websites and applications, ensuring cross-browser
 							compatibility.
 						</li>
-						<li class="text-dark-purple font-extralight">
+						<li class="text-dark-purple font-light">
 							Stay at the forefront of the web development
 							industry by continuously adopting emerging
 							technologies, libraries, and trends to enhance
@@ -154,12 +154,12 @@
 				</div>
 			</div>
 			<div class="education flex flex-col items-start space-y-4">
-				<h3 class="font-bold">Education</h3>
+				<h3 class="font-bold text-[20px]">Education</h3>
 				<div class="flex flex-col space-y-1">
 				<h3>
 					California Polytechnic State University, San Luis Obispo, CA
 				</h3>
-				<p class="text-dark-purple">Psychology, BS</p>
+				<p class="text-dark-purple font-light">Psychology, BS</p>
 			</div>
 			</div>
 		</div>
@@ -172,5 +172,8 @@ ul.resume {
 	li {
 		margin-left: 20px;
 	}
+}
+h4{
+	font-family: 'Roboto', serif;
 }
 </style>
